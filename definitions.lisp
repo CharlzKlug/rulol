57929fe7ee10c9bd3bbb9522e57ea5501071904d
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2013-12-08 16:29:33 +0500

    Added section Reader Security.

--- definitions.lisp
+++ definitions.lisp
@@ -110,3 +110,24 @@
 
 (defun cyclic-p (l)
   (cyclic-p-aux l (make-hash-table)))
+
+(defvar safe-read-from-string-blacklist
+  '(#\# #\: #\|))
+
+(let ((rt (copy-readtable nil)))
+  (defun safe-reader-error (stream closech)
+    (declare (ignore stream closech))
+    (error "safe-read-from-string failure"))
+  (dolist (c safe-read-from-string-blacklist)
+    (set-macro-character
+     c #'safe-reader-error nil rt))
+  (defun safe-read-from-string (s &optional fail)
+    (if (stringp s)
+	(let ((*readtable* rt) *read-eval*)
+	  (handler-bind
+	      ((error (lambda (condition)
+			(declare (ignore condition))
+			(return-from
+			 safe-read-from-string fail))))
+	    (read-from-string s)))
+	fail)))
