da447dab661a1c6a086672aed8da2b645e7112ea
Ametov Imil Ilimdarovich <charlz_klug@mail.ru>
2014-02-15 22:51:29 +0500

    Pretty printed Top-Down Programming.

--- definitions.lisp
+++ definitions.lisp
@@ -43,7 +43,19 @@
 	      ((char= ch curr))
 	    (push curr chars))
 	  (cons (coerce (nreverse chars) 'string)
-		(segment-reader stream ch (- n 1)))))))
+		(segment-reader stream ch (- n 1))))))
+
+  (defun group (source n)
+    (if (zerop n) (error "zero length"))
+    (labels ((rec (source acc)
+	       (let ((rest (nthcdr n source)))
+		 (if (consp rest)
+		     (rec rest (cons
+				(subseq source 0 n)
+				acc))
+		     (nreverse
+		      (cons source acc))))))
+      (if source (rec source nil) nil))))
 
 (defmacro defmacro! (name args &rest body)
   (let* ((os (remove-if-not #'o!-symbol-p args))
@@ -131,3 +143,102 @@
 			 safe-read-from-string fail))))
 	    (read-from-string s)))
 	fail)))
+
+(defmacro unit-of-time (value unit)
+  `(* ,value
+      ,(case unit
+	     ((s) 1)
+	     ((m) 60)
+	     ((h) 3600)
+	     ((d) 86400)
+	     ((ms) 1/1000)
+	     ((us) 1/1000000))))
+
+(defmacro! defunits% (quantity base-unit &rest units)
+  `(defmacro ,(symb 'unit-of- quantity) (,g!val ,g!un)
+     `(* ,,g!val
+	 ,(case ,g!un
+		((,base-unit) 1)
+		,@(mapcar (lambda (x)
+			    `((,(car x)) ,(cadr x)))
+			  (group units 2))))))
+
+(defun defunits-chaining% (u units)
+  (let ((spec (find u units :key #'car)))
+    (if (null spec)
+	(error "Unknown unit ~a" u)
+	(let ((chain (cadr spec)))
+	  (if (listp chain)
+	      (* (car chain)
+		 (defunits-chaining%
+		     (cadr chain)
+		     units))
+	      chain)))))
+
+(defmacro! defunits%% (quantity base-unit &rest units)
+  `(defmacro ,(symb 'unit-of- quantity) (,g!val ,g!un)
+     `(* ,,g!val
+	 ,(case ,g!un
+		((,base-unit) 1)
+		,@(mapcar (lambda (x)
+			    `((,(car x))
+			      ,(defunits-chaining%
+				(car x)
+				(cons `(,base-unit 1)
+				      (group units 2)))))
+			  (group units 2))))))
+
+(defun defunits-chaining (u units prev)
+  (if (member u prev)
+      (error "~{ ~a~^ depends on~}"
+	     (cons u prev)))
+  (let ((spec (find u units :key #'car)))
+    (if (null spec)
+	(error "Unknown unit ~a" u)
+	(let ((chain (cadr spec)))
+	  (if (listp chain)
+	      (* (car chain)
+		 (defunits-chaining
+		     (cadr chain)
+		     units
+		   (cons u prev)))
+	      chain)))))
+
+(defmacro! defunits (quantity base-unit &rest units)
+  `(defmacro ,(symb 'unit-of- quantity)
+       (,g!val ,g!un)
+     `(* ,,g!val
+	 ,(case ,g!un
+		((,base-unit) 1)
+		,@(mapcar (lambda (x)
+			    `((,(car x))
+			      ,(defunits-chaining
+				(car x)
+				(cons
+				 `(,base-unit 1)
+				 (group units 2))
+				nil)))
+			  (group units 2))))))
+
+(defunits distance m
+  km 1000
+  cm 1/100
+  mm (1/10 cm)
+  nm (1/1000 mm)
+
+  yard 9144/10000 ; Принято в 1956
+  foot (1/3 yard)
+  inch (1/12 foot)
+  mile (1760 yard)
+  furlong (1/8 mile)
+
+  fathom (2 yard) ; Принято в 1929
+  nautical-mile 1852
+  cable (1/10 nautical-mile)
+  
+  old-brit-nautical-mile ; Отменено в 1970
+  (6080/3 yard)
+  old-brit-cable
+  (1/10 old-brit-nautical-mile)
+  old-brit-fathom
+  (1/100 old-brit-cable))
